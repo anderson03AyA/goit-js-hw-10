3cc82bbd2cb7a04decdf6d485842739fd63250c3
anderson03AyA <andersonsolarte03@gmail.com>
2023-04-23 02:45:52 -0500

    Api Paises
    404 notFound falta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,86 @@
 import './css/styles.css';
+import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputText = document.getElementById('search-box');
+const ul = document.querySelector('.country-list');
+const div = document.querySelector('.country-info');
+
+const API_URL = `https://restcountries.com/v3.1/name/`;
+
+const debounce = (callback, delay) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => callback(...args), delay);
+  };
+};
+
+const handleInput = event => {
+  if (inputText.value === '') {
+      Notify.warning(`❌ Rejected null`);
+      
+            ul.style.display = 'none';
+            div.style.display = 'none';
+  } else {
+    fetch(`${API_URL}${inputText.value}`)
+      .then(response => response.json())
+      .then(users => {
+        if (users.length >= 2 && users.length <= 10) {
+          const tpl = users.map(
+            user =>
+              `<li><img src="${user.flags.svg}" alt="Flag of ${user.name.common}" width="50" height="30">${user.name.common}</li>`
+          );
+            ul.style.display = 'flex';
+            div.style.display = 'none'
+            ul.innerHTML = `<ul>${tpl}</ul>`;
+            
+        }
+        else if (users.length > 10) {
+          Notify.info(
+            `"Too many matches found. Please enter a more specific name."`
+          );
+        }
+        else if(users.length===1){
+            const tpl = users.map(
+              user =>
+                `
+                <div class="list"> 
+                <img src="${user.flags.svg}"
+                 alt="Flag of ${user.name.common}" 
+                 width="50" height="30">${user.name.common}
+                 </div>
+                 <div class="list"> <br>
+                 <h6>Capital: </h6>   ${user.capital} 
+                 </div>
+                 <div class="list"> 
+                 <h6>Population: </h6>   ${user.population} 
+                 </div>
+                 <div class="list"> 
+                 <h6>Languages: </h6>   ${Object.values(user.languages).join(
+                   ', '
+                 )}
+                 </div> `
+              //languages es un objeto por lo que pueden haber varios idiomas
+            );
+             div.style.display = 'flex';
+             ul.style.display = 'none';
+            div.innerHTML = `${tpl}`;
+        }
+          
+          
+      });
+  }
+};
+
+const debouncedHandleInput = debounce(handleInput, DEBOUNCE_DELAY);
+inputText.addEventListener('input', debouncedHandleInput);
+
+const numberChildValid = numberOfChildren => {
+  console.log(numberOfChildren);
+  if (numberOfChildren > 0) {
+    console.log(numberOfChildren);
+    return true;
+  } else return false;
+};
